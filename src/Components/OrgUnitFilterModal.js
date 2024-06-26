638e943b820e476cd30a4de22784086f38c2aa73
Redet Getachew <redetg@gmail.com>
2024-06-26 14:50:57 +0300

    fix: we don't want the items to be loaded on click. The first levels could be loaded automatically

--- src/Components/OrgUnitFilterModal.js
+++ src/Components/OrgUnitFilterModal.js
@@ -4,8 +4,9 @@ import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
-import OrgUnitFilter from "./OrgUnitFilter"; 
+import OrgUnitFilter from "./OrgUnitFilter";
 import { Typography, Box, CircularProgress } from "@mui/material";
+import { useEffect } from "react";
 
 const OrgUnitFilterModal = () => {
   const [open, setOpen] = useState(false);
@@ -13,7 +14,6 @@ const OrgUnitFilterModal = () => {
   const [orgUnitGroups, setOrgUnitGroups] = useState([]);
   const [orgUnitLevels, setOrgUnitLevels] = useState([]);
   const apiBase = "https://hmis.dhis.et/";
-
 
   const fetchData = async () => {
     const url = `${apiBase}api/40/organisationUnits/b3aCK1PTn5S?fields=displayName, path, id, children%5Bid%2Cpath%2CdisplayName%5D`;
@@ -27,28 +27,26 @@ const OrgUnitFilterModal = () => {
   };
 
   const fetchOrgUnitGroups = async () => {
-    const url = `${apiBase}api/organisationUnitGroups?paging=false`
+    const url = `${apiBase}api/organisationUnitGroups?paging=false`;
     const response = await fetch(url);
-    if (!response.ok){
-      throw new Error("Failed to fetch data")
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
     }
 
     const data = await response.json();
     setOrgUnitGroups(data.organisationUnitGroups);
-   
-  }
+  };
 
-  const fetchOrgUnitLevels = async() => {
+  const fetchOrgUnitLevels = async () => {
     const url = `${apiBase}api/organisationUnitLevels?paging=false`;
     const response = await fetch(url);
-    if (!response.ok){
-      throw new Error("Failed to fetch data")
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
     }
 
     const data = await response.json();
     setOrgUnitLevels(data.organisationUnitLevels);
-    
-  }
+  };
 
   const hasChildren = async (nodes) => {
     for (const node of nodes) {
@@ -65,10 +63,13 @@ const OrgUnitFilterModal = () => {
     return nodes;
   };
 
-  const handleClickOpen = () => {
+  useEffect(() => {
     fetchData();
     fetchOrgUnitGroups();
     fetchOrgUnitLevels();
+  }, []);
+
+  const handleClickOpen = () => {
     setOpen(true);
   };
 
@@ -90,7 +91,11 @@ const OrgUnitFilterModal = () => {
         </DialogTitle>
         <DialogContent dividers>
           {data ? (
-            <OrgUnitFilter data={data} orgUnitGroups={orgUnitGroups} orgUnitLevels={orgUnitLevels}/>
+            <OrgUnitFilter
+              data={data}
+              orgUnitGroups={orgUnitGroups}
+              orgUnitLevels={orgUnitLevels}
+            />
           ) : (
             <CircularProgress size={24} />
           )}
