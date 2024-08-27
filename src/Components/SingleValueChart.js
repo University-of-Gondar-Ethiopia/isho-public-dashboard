a6cc97c642f8cad39fb69187e178b661474e5247
Redet Getachew <redetg@gmail.com>
2024-08-27 17:54:17 +0300

    fix: #57 Single value failing on no data

--- src/Components/SingleValueChart.js
+++ src/Components/SingleValueChart.js
@@ -2,29 +2,60 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 
 const SingleValueChart = ({ chartData }) => {
-  let value = chartData?.rows[0][1];
-  let dataElement = chartData?.rows[0][0];
+  try {
+    let value = chartData?.rows[0][1];
+    let dataElement = chartData?.rows[0][0];
 
-  let title =
-    chartData &&
-    chartData.rows &&
-    chartData.rows[0] &&
-    dataElement &&
-    chartData.metaData.items[dataElement]
-      ? chartData.metaData.items[dataElement]?.name
-      : "";
+    let title =
+      chartData &&
+      chartData.rows &&
+      chartData.rows[0] &&
+      dataElement &&
+      chartData.metaData.items[dataElement]
+        ? chartData.metaData.items[dataElement]?.name
+        : "";
 
-  let dataElementMetadata =
-    chartData && chartData.metaData.items[dataElement]
-      ? chartData.metaData.items[dataElement]
-      : "";
+    let dataElementMetadata =
+      chartData && chartData.metaData.items[dataElement]
+        ? chartData.metaData.items[dataElement]
+        : "";
 
-  let text = "";
+    let text = "";
 
-  console.log("chart data", chartData, dataElementMetadata);
+    console.log("chart data", chartData, dataElementMetadata);
 
-  if (dataElementMetadata?.dimensionItemType == "INDICATOR") {
-    if (dataElementMetadata?.indicatorType.factor == 100) {
+    if (dataElementMetadata?.dimensionItemType == "INDICATOR") {
+      if (dataElementMetadata?.indicatorType.factor == 100) {
+        text = (
+          <Typography
+            display="flex"
+            alignItems="center"
+            component="div"
+            variant="h1"
+            color="primary"
+          >
+            {value + "%"}
+          </Typography>
+        );
+      } else {
+        text = (
+          <>
+            <Typography
+              display="flex"
+              alignItems="center"
+              component="div"
+              variant="h1"
+              color="primary"
+            >
+              {value}
+            </Typography>
+            <Typography>
+              {" " + dataElementMetadata?.indicatorType?.name}
+            </Typography>
+          </>
+        );
+      }
+    } else {
       text = (
         <Typography
           display="flex"
@@ -33,55 +64,28 @@ const SingleValueChart = ({ chartData }) => {
           variant="h1"
           color="primary"
         >
-          {value + "%"}
+          {value}
         </Typography>
       );
-    } else {
-      text = (
-        <>
-          <Typography
-            display="flex"
-            alignItems="center"
-            component="div"
-            variant="h1"
-            color="primary"
-          >
-            {value}
-          </Typography>
-          <Typography>
-            {" " + dataElementMetadata?.indicatorType?.name}
-          </Typography>
-        </>
-      );
     }
-  } else {
-    text = (
-      <Typography
-        display="flex"
-        alignItems="center"
-        component="div"
-        variant="h1"
-        color="primary"
-      >
-        {value}
-      </Typography>
-    );
-  }
 
-  return (
-    <div
-      style={{
-        minHeight: "100%",
-        alignItems: "center",
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      {text}
-      <Typography>{title}</Typography>
-    </div>
-  );
+    return (
+      <div
+        style={{
+          minHeight: "100%",
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        {text}
+        <Typography>{title}</Typography>
+      </div>
+    );
+  } catch (error) {
+    return "No data";
+  }
 };
 
 export default SingleValueChart;
