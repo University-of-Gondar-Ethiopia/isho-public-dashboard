611257c146ece8a67f73569a5e5d54e252d6eef2
Redet Getachew <redetg@gmail.com>
2024-09-09 14:26:00 +0300

    removes old dashboard selector

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -114,56 +114,25 @@ export default function Chart({
     orgunitFilters,
     orgunitGroupFilters,
     orgunitLevelFilters,
-    hideEmptyCharts
+    hideEmptyCharts,
+    indicator,
+    phase
   ) => {
     setFilters({
       orgunits: orgunitFilters,
       orgunitGroup: orgunitGroupFilters,
       orgunitLevel: orgunitLevelFilters,
       hideEmptyCharts: hideEmptyCharts,
+      indicator: indicator,
+      phase: phase,
     });
   };
 
   return (
     <React.Fragment>
-      <Grid item xs={12} md={10} lg={10}>
-        <Paper
-          sx={{
-            p: 2,
-            display: "flex",
-            flexDirection: "row",
-            height: "400",
-            flexWrap: "nowrap",
-            justifyContent: "flex-end",
-            alignItems: "center",
-            gap: "2%",
-          }}
-        >
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">
-              {"Select Dashboard"}
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={dashboard?.id}
-              label="Select Dashboard"
-              onChange={handleChartChange}
-            >
-              {loading ? (
-                <MenuItem disabled>
-                  <CircularProgress size={24} />
-                </MenuItem>
-              ) : (
-                dashboardMenuList()
-              )}
-            </Select>
-          </FormControl>
-        </Paper>
-      </Grid>
-
       <Filters
         dashboard={dashboard}
+        filters={filters}
         handelFilterSelect={handelFilterSelect}
         loading={loading}
         dashboardMenuList={dashboardMenuList}
